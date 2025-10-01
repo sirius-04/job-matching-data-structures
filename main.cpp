cb0f68e51ff1f2bf8749065d18cbb3e8c3bd0d47
yongliang <140040477+sirius-04@users.noreply.github.com>
2025-10-01 13:37:19 +0800

    menu skeleton

--- main.cpp
+++ main.cpp
@@ -2,11 +2,56 @@
 #include "JobLinkedList/JobLinkedList.h"
 using namespace std;
 
+void runArray() {
+    cout << "array" << endl;
+}
+
+void runLinkedList() {
+    cout << "linkedlist: " << endl;
+}
+
+void summary() {
+    cout << "summary" << endl;
+}
+
+void displayMenu() {
+    int userChoice;
+
+    while (true) {
+        cout << "\nSelect operation: " << endl;
+        cout << "[1] Array" << endl;
+        cout << "[2] Linked List" << endl;
+        cout << "[3] Summary" << endl;
+        cout << "[4] Exit" << endl;
+        cout << "Enter choice: ";
+
+        if (!(cin >> userChoice)) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid input! Please enter a number." << endl;
+            continue;
+        }
+
+        switch(userChoice) {
+            case 1:
+                runArray();
+                break;
+            case 2:
+                runLinkedList();
+                break;
+            case 3:
+                summary();
+                break;
+            case 4:
+                cout << "Exiting program..." << endl;
+                return;
+            default:
+                cout << "Please enter a valid option!" << endl;
+        }
+    }
+}
+
+
 int main() {
-    JobLinkedList* jobs = new JobLinkedList("1", "software engineer");
-
-    jobs->append("2", "data");
-    jobs->printList();
-
-    return 0;
+    displayMenu();
 }
