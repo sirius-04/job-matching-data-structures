39b80a7ae4eb9f679f50d7a160d2142fc3ffefec
TAN PO HUI <phxx04@gmail.com>
2025-10-08 13:06:59 +0800

    CSV Loader

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include "LinkedList/JobLinkedList/JobLinkedList.hpp"
 #include "LinkedList/ResumeLinkedList/ResumeLinkedList.hpp"
+#include "utils/CSVLoader/CSVLoader.hpp"
+
 using namespace std;
 
 void runArray()
@@ -63,135 +65,14 @@ void displayMenu()
     }
 }
 
-void createJobLinkedList(JobLinkedList &jobLinkedList)
-{
-    ifstream file("./dataset/cleaned_jobDescription.csv");
-
-    if (!file.is_open())
-    {
-        cerr << "Error: Could not open file.\n";
-        return;
-    }
-
-    string line;
-    getline(file, line); // skip header
-
-    while (getline(file, line))
-    {
-        stringstream ss(line);
-        string idStr, position, skillStr;
-
-        getline(ss, idStr, ',');
-        getline(ss, position, ',');
-        getline(ss, skillStr, '\n');
-
-        idStr = JobLinkedList::cleanString(idStr);
-        position = JobLinkedList::cleanString(position);
-        skillStr = JobLinkedList::cleanString(skillStr);
-
-        int id = 0;
-        try
-        {
-            id = stoi(idStr);
-        }
-        catch (...)
-        {
-            cerr << "Warning: invalid ID \"" << idStr << "\", skipping.\n";
-            continue;
-        }
-
-        stringstream sk(skillStr);
-        string skill;
-        string tempSkills[100];
-        int count = 0;
-
-        while (getline(sk, skill, ','))
-        {
-            skill = JobLinkedList::cleanString(skill);
-            if (!skill.empty())
-                tempSkills[count++] = skill;
-        }
-
-        string *skills = new string[count];
-        for (int i = 0; i < count; i++)
-            skills[i] = tempSkills[i];
-
-        jobLinkedList.append(id, position, skills, count);
-
-        delete[] skills;
-    }
-
-    file.close();
-}
-
-void createResumeLinkedList(ResumeLinkedList &resumeLinkedList)
-{
-    ifstream file("./dataset/cleaned_resume.csv");
-
-    if (!file.is_open())
-    {
-        cerr << "Error: Could not open file.\n";
-        return;
-    }
-
-    string line;
-    getline(file, line); // skip header line
-
-    while (getline(file, line))
-    {
-        stringstream ss(line);
-        string idStr, skillStr;
-
-        getline(ss, idStr, ',');
-        getline(ss, skillStr, '\n');
-
-        idStr = ResumeLinkedList::cleanString(idStr);
-        skillStr = ResumeLinkedList::cleanString(skillStr);
-
-        int id = 0;
-        try
-        {
-            id = stoi(idStr);
-        }
-        catch (...)
-        {
-            cerr << "Warning: invalid ID \"" << idStr << "\", skipping.\n";
-            continue;
-        }
-
-        // Parse skills (comma separated)
-        stringstream sk(skillStr);
-        string skill;
-        string tempSkills[100];
-        int count = 0;
-
-        while (getline(sk, skill, ','))
-        {
-            skill = ResumeLinkedList::cleanString(skill);
-            if (!skill.empty())
-                tempSkills[count++] = skill;
-        }
-
-        string *skills = new string[count];
-        for (int i = 0; i < count; i++)
-            skills[i] = tempSkills[i];
-
-        resumeLinkedList.append(id, skills, count);
-
-        delete[] skills;
-    }
-
-    file.close();
-}
-
 int main()
 {
     JobLinkedList jobLinkedList;
     ResumeLinkedList resumelinkedList;
 
-    createResumeLinkedList(resumelinkedList);
-    // createJobLinkedList(jobLinkedList);
+    // createResumeLinkedList(resumelinkedList);
+    createJobLinkedList(jobLinkedList);
 
-    // jobLinkedList.printSlice();
-    resumelinkedList.printSlice();
+    jobLinkedList.printSlice();
+    // resumelinkedList.printSlice();
 }
---
+++ utils/CSVLoader/CSVLoader.hpp
@@ -0,0 +1,11 @@
+#ifndef CSVLOADER_HPP
+#define CSVLOADER_HPP
+
+#include "../../LinkedList/JobLinkedList/JobLinkedList.hpp"
+#include "../../LinkedList/ResumeLinkedList/ResumeLinkedList.hpp"
+
+// Function declarations
+void createJobLinkedList(JobLinkedList &jobLinkedList);
+void createResumeLinkedList(ResumeLinkedList &resumeLinkedList);
+
+#endif
